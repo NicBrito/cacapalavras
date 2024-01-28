b9e0bfb3a1292396e7177a7a10d834ed57ca0d56
Nicolas Brito <nicolas_brito_barros@hotmail.com>
2024-01-27 23:19:22 -0300

    Accept user inputs

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import random
 
-# lista com as palavras que devem ser encontradas
-palavras = ["amor", "paz", "raiva", "python", "lar"]
+# definindo tamanho da matriz
+tamanho_matriz = int(input("Defina o tamanho da matriz quadrada: "))
+tamanho_linhas_matriz = tamanho_matriz
+tamanho_colunas_matriz = tamanho_matriz
+
+# criando lista de palavras
+palavras = []
+print("Você deve digitar até ", tamanho_matriz, " palavras de tamanho ", tamanho_matriz)
+for x in range(0, tamanho_matriz):
+    digite_palavra = 1
+    while(digite_palavra == 1):
+        palavra_digitada = str(input(f'Digite a {x+1}a palavra: '))
+        if(len(palavra_digitada) <= tamanho_matriz):
+            palavras.append(palavra_digitada)
+            digite_palavra = 0
+        else:
+            print("A palavra possui um tamanho não permitido!")
+            digite_palavra = 1
 
 # lista com as letras do alfabeto
 letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
           'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
           'u', 'v', 'w', 'x', 'y', 'z']
-
-# definindo tamanho da matriz
-tamanho_linhas_matriz = 6
-tamanho_colunas_matriz = 6
 
 # criando a matriz com letras aleatorias
 matriz = []

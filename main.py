adc811bfa522fb5f33ad6778eaf53c53ebcd460d
Nicolas Brito <nicolas_brito_barros@hotmail.com>
2024-01-29 03:41:08 -0300

    Horizontal word placement without overwriting

--- main.py
+++ main.py
@@ -43,14 +43,43 @@ for numero_linha in range(0, tamanho_colunas_matriz):
 
 # colocando as palavras na matriz
 for palavra in palavras:
-    tamanho_palavra = len(palavra)
-    direcao = random.choice(["horizontal", "vertical", "diagonal_para_direita", "diagonal_para_esquerda"])
+    tamanho_palavra = len(palavra) # tamanho da palavra
+    # direcao = random.choice(["horizontal", "vertical", "diagonal_para_direita", "diagonal_para_esquerda"])
+    direcao = "horizontal"
     if(direcao == "horizontal"):
-        linha_palavra = random.randint(0, tamanho_colunas_matriz-1)
-        coluna_palavra = random.randint(0, tamanho_linhas_matriz-tamanho_palavra)
-        for letra in palavra:
-            matriz[linha_palavra][coluna_palavra] = letra
-            coluna_palavra += 1
+        escolher_posicao = True
+        while(escolher_posicao == True):
+            for linha in matriz:
+                print(linha)
+            # escolhendo posicao aleatoria para a palavra
+            linha_palavra = random.randint(0, tamanho_colunas_matriz-1)
+            coluna_palavra = random.randint(0, tamanho_linhas_matriz-tamanho_palavra)
+            # checar palavra que já existe na matriz
+            palavra_existente = ""
+            linha_checagem = linha_palavra
+            coluna_checagem = coluna_palavra
+            for indice in range(0, tamanho_palavra):
+                palavra_existente += matriz[linha_checagem][coluna_checagem]
+                coluna_checagem += 1
+            print(palavra_existente)
+            # checar se a palavra não vai sobrepor outra palavra
+            palavra_existe = False
+            palavras_checagem = palavras
+            for palavra_checagem in palavras_checagem:
+                palavra_existe = palavra_existente in palavra_checagem
+                print(palavra_existente, "->", palavra_checagem, "=", palavra_existe)
+                input()
+                if palavra_existe == True:
+                    break
+            # se a palavra for sobrescrever outra, escolher nova posicao
+            if(palavra_existe == True):
+                escolher_posicao = True
+            # se a palavra não for sobrescrever outra, colocar na matriz
+            else:
+                escolher_posicao = False
+                for letra in palavra:
+                    matriz[linha_palavra][coluna_palavra] = letra
+                    coluna_palavra += 1
     elif(direcao == "vertical"):
         linha_palavra = random.randint(0, tamanho_colunas_matriz-tamanho_palavra)
         coluna_palavra = random.randint(0, tamanho_linhas_matriz-1)
